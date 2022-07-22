8f4f82887007e426304798b4e5d36fedf18469dc
Quan <s3893635>
2022-07-22 09:49:59 +0700

    Initial Commit

---
+++ TestingGits/TestingGitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGitsApp.swift
+//  TestingGits
+//
+//  Created by QuanChe on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingGitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
